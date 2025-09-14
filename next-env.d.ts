0e1dd8215698f73c4f33c6380d7f540e3f4daf9c
skatehubba <skatehubba.ai@gmail.com>
2025-09-14 04:39:55 +0000

    Add Next.js config and TypeScript setup

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// It ensures Next.js types are picked up by the TypeScript compiler
